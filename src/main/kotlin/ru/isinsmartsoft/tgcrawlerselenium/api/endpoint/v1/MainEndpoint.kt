0302da08ab985a13c7fd66a5b090c9b82f6e0787
krylov-sergey-sergeevich <krylov.sergey.sergeevich@gmail.com>
2024-08-29 14:26:03 +0300

    Fix ktlint

--- src/main/kotlin/ru/isinsmartsoft/tgcrawlerselenium/api/endpoint/v1/MainEndpoint.kt
+++ src/main/kotlin/ru/isinsmartsoft/tgcrawlerselenium/api/endpoint/v1/MainEndpoint.kt
@@ -6,17 +6,12 @@ import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import ru.isinsmartsoft.tgcrawlerselenium.config.constants.KafkaConstants
 import ru.isinsmartsoft.tgcrawlerselenium.dao.bo.worker.OPTIONS
-import ru.isinsmartsoft.tgcrawlerselenium.dao.bo.worker.seleniumProdGridUrl
 import ru.isinsmartsoft.tgcrawlerselenium.tools.ctx.AppContextStandard
 import java.net.URL
-import java.util.UUID
 
 @RestController
 class MainEndpoint(
