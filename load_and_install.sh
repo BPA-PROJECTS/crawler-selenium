412775d3e43d886e257a4fddf337ecc046c77e76
krylov-sergey-sergeevich <krylov.sergey.sergeevich@gmail.com>
2024-08-29 14:31:21 +0300

    Fix load_and_install.sh

--- load_and_install.sh
+++ load_and_install.sh
@@ -9,9 +9,18 @@ gradle clean
 # Собирает проект Gradle, компилируя исходный код и создавая артефакты.
 gradle build
 
-if docker images | awk '{print $1}' | grep -q "^back-tg-crawler-selenium$"; then
-    docker rmi -f back-tg-crawler-selenium
-fi
+docker-compose down
+
+# Удаление контейнеров
+for container in $(docker ps -a | grep 'tg-crawler-selenium' | awk '{print $1}'); do
+    docker stop $container
+    docker rm $container
+done
+
+# Удаление образов
+for image in $(docker images | grep 'tg-crawler-selenium' | awk '{print $3}'); do
+    docker rmi $image
+done
 
 docker-compose down
 docker-compose up -d --force-recreate
